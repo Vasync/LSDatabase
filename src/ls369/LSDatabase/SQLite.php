317cf809a0b6c23519d148cedba7a66cc0cfa0e9
LSpace <tbao12901@gmail.com>
2024-07-26 16:09:20 +0700

    Update SQLite.php existsTable function

--- src/ls369/LSDatabase/SQLite.php
+++ src/ls369/LSDatabase/SQLite.php
@@ -11,20 +11,32 @@ class SQLite extends SQLite3 {
     
     public function createTable(string $name, array $columns): self {
         $columnsStr = implode(", ", array_map(fn($col) => "$col TEXT", $columns));
-        $query = "CREATE TABLE IF NOT EXISTS $name ($columnsStr)";
+        $query = "CREATE TABLE IF NOT EXISTS $name ($columnsStr);";
+        
         $this->exec($query);
+        
         return $this;
     }
 
     public function removeTable(string $name): void {
         $query = "DROP TABLE IF EXISTS $name";
+        
         $this->exec($query);
+    }
+
+    public function existsTable(string $name): bool {
+        $query = "SELECT name FROM sqlite_master WHERE type='table' AND name='table_name';";
+        
+        $result = $this->exec($query);
+
+        if ($result == $name) return true;
+        return false;
     }
     
     public function set(string $table, array $data): void {
         $columns = implode(", ", array_keys($data));
         $values = implode(", ", array_map(fn($value) => "'" . SQLite3::escapeString($value) . "'", array_values($data)));
-        $query = "UPDATE INTO $table ($columns) VALUES ($values)";
+        $query = "UPDATE INTO $table ($columns) VALUES ($values);";
             $this->exec($query);
         }
     
@@ -50,4 +62,5 @@ class SQLite extends SQLite3 {
         $query = "ALTER TABLE $table DROP COLUMN $column $type";
         $this->exec($query);
     }
+    
 }
