d7acb6f68b5ff2c4f14fb15edbae9866a761bc4e
LSpace <tbao12901@gmail.com>
2024-07-26 11:07:56 +0700

    Update SQLite.php

--- src/ls369/LSDatabase/SQLite.php
+++ src/ls369/LSDatabase/SQLite.php
@@ -19,7 +19,7 @@ class SQLite extends SQLite3 {
     public function set(string $table, array $data): void {
         $columns = implode(", ", array_keys($data));
         $values = implode(", ", array_map(fn($value) => "'" . SQLite3::escapeString($value) . "'", array_values($data)));
-        $query = "INSERT INTO $table ($columns) VALUES ($values)";
+        $query = "UPDATE INTO $table ($columns) VALUES ($values)";
             $this->exec($query);
         }
     
